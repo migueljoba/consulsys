71729df16441effa0767cf42a0f144d38851d9e3
Miguel <mjose.baez@gmail.com>
2018-07-16 22:41:54 -0400

    #1 Estructura inicial del proyecto, generada con SPRING INITIALIZR

---
+++ src/main/java/com/pr1/consulsys/ConsulsysApplication.java
@@ -0,0 +1,12 @@
+package com.pr1.consulsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulsysApplication.class, args);
+	}
+}
